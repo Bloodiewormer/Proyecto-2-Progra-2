36fe39c4dc93d606b8fe94201346912366971646
Marisol Quirós <sol22qv@gmail.com>
2025-06-06 14:02:42 -0600

    commented methods removed

--- Proyecto-2-Progra-2/Carnivore.h
+++ Proyecto-2-Progra-2/Carnivore.h
@@ -3,7 +3,6 @@
 class Carnivore : public Creature 
 {
 public:
-	//Carnivore(Position pos, IMovementStrategy* m, IFeedingStrategy* f, IReproductionStrategy* r, int energy = 100);
 	Carnivore(int x, int y, IMovementStrategy* m);
 	char getSymbol() override;
 };
--- Proyecto-2-Progra-2/RandomMovement.cpp
+++ Proyecto-2-Progra-2/RandomMovement.cpp
@@ -1,11 +1,5 @@
 #include "RandomMovement.h"
 
-//Position RandomMovement::move(const Position& current) {
-//    int dx = (std::rand() % 3) - 1; // -1, 0, 1
-//    int dy = (std::rand() % 3) - 1;
-//    return Position(current.getX() + dx, current.getY() + dy);
-//}
-
 void RandomMovement::move(Creature* creature)
 {
     int dx = rand() % 3 - 1;
